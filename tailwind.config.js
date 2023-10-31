7868f3c00c1b22e5d797d80f07745d3f645990de
HenryAprendo <hjsmipuc@gmail.com>
2023-10-31 12:03:58 -0500

    assets del proyecto loopstudios

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray-le': '#8C8C8C',
+        'very-dark-gray-le': '#696969',
+      },
+      fontFamily: {
+        alata: ['Alata', 'sans-serif'],
+        JosefinSans: ['"Josefin Sans"'],
+      },
+      backgroundImage: {
+        'banner-mobile-hero-le': "url('./src/assets/loopstudios/images/mobile/image-hero.jpg')",
+      }
+    },
   },
   plugins: [],
 }
