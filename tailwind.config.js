251fee959765839b57f2a895554f55aaee5d1a9a
HenryAprendo <hjsmipuc@gmail.com>
2023-11-10 15:46:22 -0500

    Fuentes,colores y assets para una nueva landing page insure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // loopstudios
         'dark-gray-le': '#8C8C8C',
         'very-dark-gray-le': '#696969',
+
+        // ensure
+        'dark-violet-ensure':'#2D2640',
+        'grayish-blue-ensure': '#95A9C6',
+        'very-dark-violet-ensure':'#2B272F',
+        'dark-grayish-violet-ensure':'#837D87',
+        'very-light-gray-ensure':'#FAFAFA'
+
       },
       fontFamily: {
+        // loopstudios
         alata: ['Alata', 'sans-serif'],
         JosefinSans: ['"Josefin Sans"'],
+
+        // ensure
+        DMserifDisplay: ['DM Serif Display', 'serif'],
+        karla: ['Karla', 'sans-serif'],
+
       },
       backgroundImage: {
         'banner-mobile-hero-le': "url('./assets/loopstudios/images/mobile/image-hero.jpg')",
